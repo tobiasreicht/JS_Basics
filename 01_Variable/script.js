a6320fd5d996e41e0527a12d9e71934e4438609d
tobiasreicht <tobias.reicht@student.htldornbirn.at>
2023-12-04 11:57:52 +0100

    first commit

---
+++ 01_Variable/script.js
@@ -0,0 +1,42 @@
+console.log("Hallo"); 
+
+console.log("Tolle Sache"); 
+
+// Variablen 
+
+let a = 7; 
+
+let b = 8; 
+
+let c = 10; 
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+console.log ("a + b + c = " + (a + b + c)) // a + b + c 
+
+console.log ("(a+b)/c = " + ((a+b)/c))     // (a + b) / c 
+
+console.log ("c-a+b = " + (c-a+b))// c -a + b 
+
+
+console.log ("a+b+c*3=" + (a+b+c*3))// a + b + c * 3 
+
+console.log ("(a+b+c)/4 = " + ((a+b+c)/4))// (a + b + c)/4 
+
+ 
+
+// Ändere die Variablen auf folgende Werte 
+
+a= 3;        // a = 3
+
+b= a+3;      // b = a + 3 
+
+c= c-3;     // c = c -3 
+
+
+ // gib die neuen Werte der Variablen aus 
+
+ console.log (a);
+ console.log (b);
+ console.log (c);
+ 
